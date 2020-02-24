12e57f9d7412e8b8d27cbd27ce0a526d22b48b75
meganshermer <49258236+meganshermer@users.noreply.github.com>
2020-02-24 14:09:41 -0800

    illustration
    got the illustration in just need to connect gsap

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log('OK');
+gsap.from('#disco', {  
+ 
+    scale: 3, 
+    rotation: 360, 
+    duration: 4,
+    repeat: 100,
+  });
+  
+  
+  gsap.from('#record', {  
+   
+    rotation: 360, 
+    duration: 1.5,
+    repeat: 100,
+  });
+  
